1171cb0656c5677fba5ab54b4b5c7e13e9f91da0
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2022-01-07 17:05:16 -0500

    feat: added heart-like

---
+++ likeHeart.js
@@ -0,0 +1,15 @@
+const heartLike = document.querySelector(".like-heart");
+const heartUnlike = document.querySelector(".unlike-heart");
+let textLikes = document.querySelector(".likes");
+
+heartLike.addEventListener("click", () => {
+  textLikes.innerHTML = "3 likes";
+  heartUnlike.classList.remove("display-none");
+  heartUnlike.classList.add("heart-active");
+  heartLike.classList.add("display-none");
+});
+heartUnlike.addEventListener("click", () => {
+  textLikes.innerHTML = "2 likes";
+  heartLike.classList.remove("display-none");
+  heartUnlike.classList.add("display-none");
+});
