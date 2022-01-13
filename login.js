c20ac7fe5c37dc22e0127abbdce5399daa3c5c75
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2022-01-13 09:27:29 -0500

    cleaning

--- login.js
+++ login.js
@@ -3,57 +3,62 @@ const loginPassword = document.getElementById("login-password");
 const loginButton = document.getElementById("login-button");
 const showPassword = document.querySelector(".show-password");
 const loginIncorrect = document.querySelector(".login-incorrect");
-
 const userID = "insta";
 const userPassword = "123456";
 
 // ====== Login button colour changes ======
-loginID.addEventListener("input", (e) => {
+const checkLoginID = (e) => {
   let inputValue = e.target.value;
   if (inputValue.length > 0 && loginPassword.value.length > 5) {
     loginButton.style.opacity = "1";
+    loginButton.style.cursor = "pointer";
   }
   if (inputValue.length === 0 || loginPassword.value.length < 6) {
     loginButton.style.opacity = "0.3";
+    loginButton.style.cursor = "default";
   }
-});
+};
 
-loginPassword.addEventListener("input", (e) => {
+const checkLoginPassword = (e) => {
   let passwordValue = e.target.value;
   if (loginID.value.length > 0 && passwordValue.length > 5) {
     loginButton.style.opacity = "1";
+    loginButton.style.cursor = "pointer";
   }
+
   if (loginID.value.length === 0 || passwordValue.length < 6) {
     loginButton.style.opacity = "0.3";
+    loginButton.style.cursor = "default";
   }
 
   passwordValue.length === 0
     ? (showPassword.style.display = "none")
     : (showPassword.style.display = "block");
-});
+};
+
+loginID.addEventListener("input", checkLoginID);
+loginPassword.addEventListener("input", checkLoginPassword);
 
 // ====== Show/hide password ======
-showPassword.addEventListener("click", (e) => {
+const showHidePassword = (e) => {
   e.preventDefault();
-
   loginPassword.type !== "password"
     ? (loginPassword.type = "password")
     : (loginPassword.type = "text");
-
   loginPassword.type === "password"
     ? (showPassword.textContent = "Show")
     : (showPassword.textContent = "Hide");
-});
+};
+
+showPassword.addEventListener("click", showHidePassword);
 
 // ====== Login test ======
-loginButton.addEventListener("click", (e) => {
+const signIn = (e) => {
   e.preventDefault();
-  if (
-    loginID.value.toLowerCase() === userID.toLowerCase() &&
-    loginPassword.value === userPassword
-  ) {
-    window.location = "main.html";
-  } else {
-    loginIncorrect.style.display = "block";
-  }
-});
+  loginID.value.toLowerCase() === userID.toLowerCase() &&
+  loginPassword.value === userPassword
+    ? (window.location = "main.html")
+    : (loginIncorrect.style.display = "block");
+};
+
+loginButton.addEventListener("click", signIn);
