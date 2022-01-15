aefbda622527c5a3cdc9ae5533dcc82efc4efbfb
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2022-01-15 12:56:11 -0500

    cleaning code

--- likeHeart.js
+++ likeHeart.js
@@ -1,6 +1,6 @@
 const heartLike = document.querySelector(".like-heart");
 const heartUnlike = document.querySelector(".unlike-heart");
-let textLikes = document.querySelector(".likes");
+const textLikes = document.querySelector(".likes");
 
 heartLike.addEventListener("click", () => {
   textLikes.innerHTML = "3 likes";
@@ -8,6 +8,7 @@ heartLike.addEventListener("click", () => {
   heartUnlike.classList.add("heart-active");
   heartLike.classList.add("display-none");
 });
+
 heartUnlike.addEventListener("click", () => {
   textLikes.innerHTML = "2 likes";
   heartLike.classList.remove("display-none");
