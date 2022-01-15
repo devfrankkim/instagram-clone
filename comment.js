72d1da372defff584665d7f9f435f2ee869b45d7
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2022-01-15 12:55:06 -0500

    feat: added delete comments function

--- comment.js
+++ comment.js
@@ -2,17 +2,38 @@ const commentBox = document.querySelector(".comment-box");
 const addComment = document.querySelector(".add-form");
 const typeComment = document.querySelector(".type-comment");
 const postButton = document.querySelector(".add-comments .post");
+const comment = document.querySelector(".comment-style");
+
+const deleteComment = (e) => {
+  if (e) {
+    const box = e.target.parentNode.parentNode;
+    box.removeChild(e.target.parentNode);
+  }
+};
 
 const createComments = (e) => {
   e.preventDefault();
   if (typeComment.value.length > 0) {
     postButton.style.opacity = "0.3";
-    let comment = document.createElement("div");
+    const comment = document.createElement("div");
+    comment.style.display = "flex";
     comment.classList.add("comment-style");
-    comment.innerHTML = `<span style="font-weight:bold"> swe.frank.kim </span> ${typeComment.value}`;
+    comment.innerHTML = `
+      <div>
+        <span style="font-weight:bold;"> swe.frank.kim </span> 
+        <span> ${typeComment.value} <span>
+      </div>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;       
+      
+      `;
+    const newDeleteButton = document.createElement("button");
+    newDeleteButton.textContent = " X ";
+    newDeleteButton.classList.add("delete-button");
+    comment.appendChild(newDeleteButton);
     commentBox.appendChild(comment);
     typeComment.value = "";
     postButton.classList.remove("pointer");
+    newDeleteButton.addEventListener("click", (e) => deleteComment(e));
   }
 };
 
