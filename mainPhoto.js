979cd0f08603e9d9c4927d348948ef8e92f6a018
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2022-01-15 12:56:31 -0500

    cleaning code

--- mainPhoto.js
+++ mainPhoto.js
@@ -3,9 +3,7 @@ let images = [
   "https://www.instagram.com/static/images/homepage/screenshot4.jpg/842fe5699220.jpg",
   "https://www.instagram.com/static/images/homepage/screenshot5.jpg/0a2d3016f375.jpg",
 ];
-
 let index = 0;
-
 const imgList = document.querySelector(".mainPhoto");
 
 function changeImg() {
