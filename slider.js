aac2ed698f4fa34811e6636b0e31998309c50c45
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2022-01-15 12:56:44 -0500

    cleaning code

--- slider.js
+++ slider.js
@@ -1,5 +1,5 @@
 // ============= Content Slider =============
-let IMAGES = [
+const IMAGES = [
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSK7kb76FiUc80WNVG5kjD4BxxiC3VeuS4Jow&usqp=CAU",
   "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F23%2F2020%2F04%2F27%2Fpositive-quotes-captions-life-motivational-inspirational-jim-rohn.jpg",
   "https://media.istockphoto.com/vectors/calm-down-simple-inspire-and-motivational-quote-english-idiom-youth-vector-id1087183038?k=20&m=1087183038&s=170667a&w=0&h=Jk2XhdKKuqJLZmbVU2qUNvRPVVFxWBL17vFGVHpOfxM=",
@@ -10,7 +10,7 @@ const contentSlider1 = document.querySelector(
 const contentSlider2 = document.querySelector(
   ".content__container-content .slider-2"
 );
-let mainImage = document.querySelector(".content__container-content img");
+const mainImage = document.querySelector(".content__container-content img");
 let index = 0;
 
 const moveLeftContent = () => {
@@ -36,13 +36,13 @@ contentSlider2.addEventListener("click", moveRightContent);
 
 // ============= Scroll - story - Slider =============
 
-let storyUlContainer = document.querySelector(
+const storyUlContainer = document.querySelector(
   ".story__container__organizer__parent"
 );
-let storySlider1 = document.querySelector(
+const storySlider1 = document.querySelector(
   ".story__container__slider-box .slider-1"
 );
-let storySlider2 = document.querySelector(
+const storySlider2 = document.querySelector(
   ".story__container__slider-box .slider-2"
 );
 
